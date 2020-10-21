9cc7848d7f64b28b12162b19ebaca54973baea94
martekyrkjebo <martebk@stud.ntnu.no>
2020-10-21 17:35:24 +0200

    Updates

---
+++ precache-manifest.dd25e5ccd978de1a396a10a84485d9c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2689fef6e5b27cccd3a082e916c957b2",
+    "url": "/webkurs2020-CICD-React/index.html"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "46ccdb70e3eaafd6abbf",
+    "url": "/webkurs2020-CICD-React/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e77625ce66444bc7b13d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webkurs2020-CICD-React/static/js/2.16ab8709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ccdb70e3eaafd6abbf",
+    "url": "/webkurs2020-CICD-React/static/js/main.32265b1a.chunk.js"
+  },
+  {
+    "revision": "580e99ae8bc13b712167",
+    "url": "/webkurs2020-CICD-React/static/js/runtime-main.89e14c0b.js"
+  }
+]);
